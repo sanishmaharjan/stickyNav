fa480e31fc092b4019ab3d8ad963e9e42f6a1780
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2016-03-29 16:12:03 +0545

    demo file added

--- jquery.stickyNav.js
+++ jquery.stickyNav.js
@@ -5,34 +5,32 @@
  * @author Sanish Maharjan https://github.com/sanishmaharjan
  * @see https://github.com/sanishmaharjan/stickyNav
  */
-(function ( $ ) {
-    $.fn.stickyNav = function ( options ) {
+(function ($) {
+    $.fn.stickyNav = function (options) {
         var $element = $(this);
         var stickyNav = {
-            lastScrollTop : 0,
-            options : $.extend({
-                style : {
-                    "top" : 0,
-                    "width" : "100%",
-                    "z-index" : 10,
-                    "background-color" : "#ffffff",
-                    "border" : "solid #cccccc",
-                    "border-width" : "0 0 1px",
-                    "box-shadow" : "0 3px 2px 0 rgba(0, 0, 0, 0.09)",
-                    "position" : "absolute"
-                }
+            lastScrollTop: 0,
+            options: $.extend({
+                "top": 0,
+                "width": "100%",
+                "z-index": 10,
+                "background-color": "#ffffff",
+                "border": "solid #cccccc",
+                "border-width": "0 0 1px",
+                "box-shadow": "0 3px 2px 0 rgba(0, 0, 0, 0.09)",
+                "position": "absolute",
+                "left": 0
             }, options),
-            init : function () {
+            init: function () {
                 $element.wrap('<div class="sticky-nav"></div>');
-                $('body').append($(".sticky-nav"));
-                $(".sticky-nav").css(options.style);
+                $(".sticky-nav").css(stickyNav.options);
                 stickyNav.addEventHandler();
             },
-            addEventHandler : function () {
-                $(window).scroll(function ( event ) {
+            addEventHandler: function () {
+                $(window).scroll(function (event) {
                     var scrollTop = $(window).scrollTop();
                     if (stickyNav.lastScrollTop > scrollTop) {
-                        $(".sticky-nav").css("top", scrollTop + stickyNav.options.style.top);
+                        $(".sticky-nav").css("top", scrollTop + stickyNav.options.top);
                     }
                     stickyNav.lastScrollTop = scrollTop;
                 });

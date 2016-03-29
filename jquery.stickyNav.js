11297f9e4ed16981be03340a62439af661a7e168
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2016-03-29 17:31:37 +0545

    bouncing menu issue fix

--- jquery.stickyNav.js
+++ jquery.stickyNav.js
@@ -13,24 +13,43 @@
             options: $.extend({
                 "top": 0,
                 "width": "100%",
-                "z-index": 10,
-                "background-color": "#ffffff",
-                "border": "solid #cccccc",
-                "border-width": "0 0 1px",
-                "box-shadow": "0 3px 2px 0 rgba(0, 0, 0, 0.09)",
-                "position": "absolute",
-                "left": 0
+                "zIndex": 10,
+                "backgroundColor": "#ffffff",
+                "scrollUp": true
             }, options),
             init: function () {
                 $element.wrap('<div class="sticky-nav"></div>');
-                $(".sticky-nav").css(stickyNav.options);
+
+                $(".sticky-nav").css({
+                    "top": stickyNav.options.top,
+                    "width": stickyNav.options.width,
+                    "z-index": stickyNav.options.zIndex,
+                    "background-color": stickyNav.options.backgroundColor,
+                    "border": "solid #cccccc",
+                    "border-width": "0 0 1px",
+                    "box-shadow": "0 3px 2px 0 rgba(0, 0, 0, 0.09)",
+                    "position": stickyNav.options.scrollUp ? "absolute" : "fixed",
+                    "left": 0
+                });
                 stickyNav.addEventHandler();
             },
             addEventHandler: function () {
                 $(window).scroll(function (event) {
                     var scrollTop = $(window).scrollTop();
                     if (stickyNav.lastScrollTop > scrollTop) {
-                        $(".sticky-nav").css("top", scrollTop + stickyNav.options.top);
+                        console.log("scroll down");
+                        if (stickyNav.options.scrollUp) {
+                            $(".sticky-nav").css("position", "fixed");
+                            $(".sticky-nav").css("top", stickyNav.options.top);
+                        }
+                    } else {
+                        console.log("scroll up");
+                        if (stickyNav.options.scrollUp) {
+                            if ($(".sticky-nav").css("position") == "fixed") {
+                                $(".sticky-nav").css("position", "absolute");
+                                $(".sticky-nav").css("top", scrollTop + stickyNav.options.top);
+                            }
+                        }
                     }
                     stickyNav.lastScrollTop = scrollTop;
                 });

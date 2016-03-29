e44f208c3639a09fb3017e85148138a95f9679db
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2016-03-29 21:40:32 +0545

    fix left positon & width issue

---
+++ README.md
@@ -0,0 +1,29 @@
+#stickyNav 1.0.3
+A jQuery plugin for sticky navigation.
+
+## Installation  
+###Bower
+````bash
+bower install stickyNav --save
+````
+
+##Usage
+````javascript
+$(function() {
+  $("#nav").stickyNav({
+      "top" : 100,
+      "zIndex": 10,
+      "backgroundColor": "#ffffff",
+      "scrollUp": false
+  });
+});
+````
+
+Options.
+
+* top : Top position of sticky navigation | default 0
+* zIndex : z-index value of sticky navigation | default 10
+* backgroundColor : sticky navigation background color | default "#ffffff"
+* scrollUp : 
+ -- If true sticky navigation will be scroll on page scroll up & appear on scroll down even page scroll not reach to top. 
+ -- If false sticky navigation always sticky to the top position.
--- jquery.stickyNav.js
+++ jquery.stickyNav.js
@@ -12,40 +12,46 @@
             lastScrollTop: 0,
             options: $.extend({
                 "top": 0,
-                "width": "100%",
                 "zIndex": 10,
                 "backgroundColor": "#ffffff",
                 "scrollUp": true
             }, options),
             init: function () {
                 $element.wrap('<div class="sticky-nav"></div>');
-
+                var leftPosition = $(".sticky-nav").offset().left;
                 $(".sticky-nav").css({
                     "top": stickyNav.options.top,
-                    "width": stickyNav.options.width,
+                    "width": $(".sticky-nav").parent().width(),
                     "z-index": stickyNav.options.zIndex,
                     "background-color": stickyNav.options.backgroundColor,
                     "border": "solid #cccccc",
                     "border-width": "0 0 1px",
                     "box-shadow": "0 3px 2px 0 rgba(0, 0, 0, 0.09)",
                     "position": stickyNav.options.scrollUp ? "absolute" : "fixed",
-                    "left": 0
+                    "left": stickyNav.options.scrollUp ? 0 : leftPosition
                 });
-                stickyNav.addEventHandler();
+                stickyNav.addEventHandler(leftPosition);
             },
-            addEventHandler: function () {
+            addEventHandler: function (leftPosition) {
                 $(window).scroll(function (event) {
                     var scrollTop = $(window).scrollTop();
                     if (stickyNav.lastScrollTop > scrollTop) {
                         if (stickyNav.options.scrollUp) {
-                            $(".sticky-nav").css("position", "fixed");
-                            $(".sticky-nav").css("top", stickyNav.options.top);
+                            $(".sticky-nav").css({
+                                    "position": "fixed",
+                                    "top": stickyNav.options.top,
+                                    "left": leftPosition
+                                }
+                            );
                         }
                     } else {
                         if (stickyNav.options.scrollUp) {
                             if ($(".sticky-nav").css("position") == "fixed") {
-                                $(".sticky-nav").css("position", "absolute");
-                                $(".sticky-nav").css("top", scrollTop + stickyNav.options.top);
+                                $(".sticky-nav").css({
+                                    "position": "absolute",
+                                    "top": scrollTop + stickyNav.options.top,
+                                    "left": 0
+                                });
                             }
                         }
                     }
